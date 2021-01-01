107e293f9d7e49db0bb8972cf0cfd5b98229f5c4
InasK6 <inaskaci@gmail.com>
2021-01-01 22:02:34 +0100

    Livre Integration Test

--- diversus/LDVH_code/src/itf/IGestLivre.java
+++ diversus/LDVH_code/src/itf/IGestLivre.java
@@ -1,6 +1,7 @@
 package itf;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public interface IGestLivre {
@@ -9,5 +10,6 @@ public interface IGestLivre {
 	public void addLivre(String nom, String auteur);
 	public ILivre getLivre(String titre);
 	public void deleteLivre(String titre);
-	
+	public List<ILivre> getLivres() ;
+	public boolean contains(String titre);
 }
