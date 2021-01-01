e395d22d090e667b277f35e9e42ceb2a610d5dfc
PALMISANO quentin <palmisanoquentin@gmail.com>
2021-01-01 17:58:56 +0100

    fonction generateHTML

--- diversus/LDVH_code/src/itf/IParser.java
+++ diversus/LDVH_code/src/itf/IParser.java
@@ -1,28 +1,32 @@
 /**
- * 
+ *
  */
 package itf;
 
-/** 
+import java.io.IOException;
+
+import composants.CLivre.Livre;
+
+/**
  * <!-- begin-UML-doc -->
  * <!-- end-UML-doc -->
  * @author inas
  * @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
  */
 public interface IParser {
-	/** 
+	/**
 	* <!-- begin-UML-doc -->
 	* <!-- end-UML-doc -->
 	* @return
 	* @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
 	*/
-	public String generateImprimable();
+	public String generateImprimable(Livre l) throws IOException;
 
-	/** 
+	/**
 	* <!-- begin-UML-doc -->
 	* <!-- end-UML-doc -->
 	* @return
 	* @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
 	*/
-	public String generateHTML();
+	public String generateHTML(Livre l) throws IOException;
 }
